a97d1603f023704288b613876cac3be78f3bebaa
smolgroot <nautilushell@protonmail.com>
2024-02-22 02:50:14 +0100

    🎨 format the code (black, isort)

--- cli.py
+++ cli.py
@@ -1,31 +1,37 @@
-import click
 import itertools
+
+import click
+from unidecode import unidecode
+
 
 def find_words(letters, wordlist):
     words = []
-    for i in range(len(letters)+1):
+    for i in range(len(letters) + 1):
         permutations = itertools.permutations(letters, i)
         for perm in permutations:
-            word = ''.join(perm)
+            word = "".join(perm)
             if word in wordlist and word not in words:
                 print(word)
                 words.append(word)
     return words
 
+
 def load_wordlist():
     with open("/usr/share/dict/american-english", "r") as file:
         return file.readlines()
 
+
 @click.command()
-@click.option('--wordlist', '-w', default='/usr/share/dict/american-english', help='A wordlist')
-@click.option('--letters', prompt='The letters',
-              help='The letters.')
+@click.option("--wordlist", "-w", default="/usr/share/dict/american-english", help="A wordlist")
+@click.option("--letters", prompt="The letters", help="The letters.")
 def main(wordlist, letters):
     dictionary = []
+    letters = unidecode(letters.upper())
     with open(wordlist, "r") as file:
         for line in file.readlines():
-            dictionary.append(line.rstrip("\n"))
+            dictionary.append(unidecode(line.rstrip("\n").upper()))
     print(find_words(letters, dictionary))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 # -*- coding: utf-8 -*-
 
+import itertools
+
 from flask import Flask, request
 from unidecode import unidecode
 
-import itertools
 
 def find_words(letters, wordlist):
     words = []
-    for i in range(len(letters)+1):
+    for i in range(len(letters) + 1):
         permutations = itertools.permutations(letters, i)
         for perm in permutations:
-            word = ''.join(perm)
+            word = "".join(perm)
             if word in wordlist and word not in words:
                 print(word)
                 words.append(word)
     return words
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
-    letters = unidecode(request.args.get('letters').upper())
+    letters = unidecode(request.args.get("letters").upper())
     if not letters:
         return "<p>No letters...</p><p>Use <a href='/?letters=stpo'>example</a></p>"
     dictionary = []
@@ -29,5 +32,6 @@ def hello_world():
             dictionary.append(unidecode(line.rstrip("\n").upper()))
     return find_words(letters, dictionary)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
