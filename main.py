3d11c51819baaca5efb27464703ccbcaa44a3270
smolgroot <nautilushell@protonmail.com>
2024-02-22 02:45:04 +0100

    🌐 normalize words (remove accents) and uppercase before compare

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from flask import Flask, request 
+# -*- coding: utf-8 -*-
+
+from flask import Flask, request
+from unidecode import unidecode
+
 import itertools
 
 def find_words(letters, wordlist):
@@ -16,13 +20,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    letters = request.args.get('letters') 
+    letters = unidecode(request.args.get('letters').upper())
     if not letters:
         return "<p>No letters...</p><p>Use <a href='/?letters=stpo'>example</a></p>"
     dictionary = []
     with open("/usr/share/dict/american-english", "r") as file:
         for line in file.readlines():
-            dictionary.append(line.rstrip("\n"))
+            dictionary.append(unidecode(line.rstrip("\n").upper()))
     return find_words(letters, dictionary)
 
 if __name__ == '__main__':
